6eb3b04a72b0e40f811699e242fcba2d6d50e952
stacysorey <stacysorey@gmail.com>
2021-01-19 06:36:03 -0500

    eupdate entry views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,8 +4,44 @@ class EntriesController < ApplicationController
     erb :'/entries/index'
   end
 
+  # create
+  post '/entry' do
+    
+    @entry = Entry.create(title:params[:title],date:params[:date],description:params[:description])
+    
+    redirect to ("/entry/#{@entry.id}")
+  end
+
+  # edit
+  get '/entry/:id/edit' do
+    @entry = Entry.find(params[:id])
+    erb :'entry/edit'
+  end
+
+  # patch
+  patch '/entry/:id' do
+    @entry = Entry.find(params[:id])
+    @entry.update(title:params[:title],date:params[:date],description:params[:description])
+    redirect "/entry/#{@entry.id}"
+  end
+
+  # show 
+  get '/entry/:id' do 
+    @entry = Entry.find(params[:id])
+    
+    erb :'entry/show' 
+  end 
+
+  # new
   get '/new_entry' do
     erb :'/entries/new'
+  end
+
+  # delete
+  delete 'entry/:id' do
+    @entry = Entry.find(params[:id])
+    @entry.destroy
+    redirect '/entry'
   end
 
 end
